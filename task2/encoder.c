dd1ad87ea3c4d16b098d8877a91972b5ebe70069
spl211 <maorbu@post.bgu.ac.il>
2022-04-05 20:49:07 +0300

    Final

--- task2/encoder.c
+++ task2/encoder.c
@@ -1,115 +1,113 @@
 #include <stdio.h>
 #include <string.h>
-
 int hexToNum(char);
 
-int main(int argc, char **argv){
-	FILE *in = stdin;
-	FILE *err = stderr;
-	FILE *out = stdout;
-	int debug = 0;
-	int addKey = 0;
-	int subKey = 0;
-	int encNum = 0;
-	int convertCount = 0;
-	int firstChar = '\0';
-	int inputFile = 0;
-	int outputFile = 0;
-	for (int i = 1; i < argc; i++){
-		if (!strcmp(argv[i], "-D")){
-		debug = 1;
-fprintf(stdout, "%s\n" , "debug activeted");
-		}
-		
-		else if (!strncmp(argv[i], "+e", 2)){
-			addKey = 1;
-			encNum = hexToNum(argv[i][2]);
-			fprintf(stdout, "%s\n" , "increase activeted");
-		}
-		else if (!strncmp(argv[i], "-e", 2)){
-            encNum = hexToNum(argv[i][2]);
-			subKey = encNum;
-			fprintf(stdout, "%s\n" , "decrease activeted");
+int main(int argc, char ** argv){
+    FILE *in = stdin;
+    FILE *err = stderr;
+    FILE* out = stdout;
+    int toFile =0;
+    int fromFile = 0;
+    int debug = 0;
+    int encSub = 0;
+    int encAdd = 0;
+    int converted = 0;
+    char firstChar = '\0';
+    int subKey =0;
+    int addKey =0;
+    int arg;
+    if (argc >1){
+        for(arg = 1; arg < argc; arg ++){
+            if(strcmp(argv[arg], "-D") == 0){
+                fprintf(err,"%s\n","debug enabled");
+                debug = 1;
+            }
+            else if(strncmp(argv[arg], "-e", 2) == 0){
+                fprintf(err,"%s\n","decrease enabled");
+                subKey =  hexToNum(argv[arg][2]);
+                encSub = subKey;
+            }else if(strncmp(argv[arg], "+e", 2) == 0){
+                fprintf(err,"%s\n","increase enabled");
+                addKey = hexToNum(argv[arg][2]);
+                encAdd = addKey;
+            }
+            else if(strncmp(argv[arg], "-i", 2) == 0){
+                printf("input is read from file\n");
+                in = fopen(argv[arg] + 2, "r");
+                fromFile =1;
+            }
+            else if(strncmp(argv[arg], "-o", 2) ==0){
+				printf("to file");
+				out = fopen(argv[arg]+2, "w");
+				toFile =1;
+			}
+        }
+    }
+    char ch;
+    while ((ch = fgetc(in)) != EOF){
+        if (subKey){
+            if (ch == 10){
+                if (firstChar == '\0')
+                    fprintf(out, "-NONE-\n");
+
+                else{
+                    firstChar = '\0';
+                    fprintf(out, "\n");
                 }
-		else if (!strncmp(argv[i], "-i", 2)){
-			inputFile = 1;
-			in = fopen(argv[i] + 2, "r");
-			fprintf(stdout, "%s\n" , "input is from file");
-		}
-		else if (!strncmp(argv[i], "-o", 2)){
-			outputFile = 1;
-				fprintf(stdout, "%s\n" , "to output");
-			out = fopen(argv[i] + 2, "w");
-		}
+                encSub = subKey;
+            }
+            else if (encSub > 0)
+                encSub --;
+            else if (encSub == 0){
+                if (firstChar == '\0')
+                    firstChar = ch;
+                fprintf(out, "%c", ch);
+            }
+        }
+        if (addKey){
+            if (firstChar == '\0')
+                firstChar = ch;
+            if (ch == 10){
+                for (int i = 0; i < encAdd; i++)
+                    fprintf(out, "%c", firstChar);
+                firstChar = '\0';
+            }
+            fprintf(out,"%c", ch);
+        }
+        if(ch >= 'A' && ch <= 'Z'){
+            if (debug) {
+                fprintf(err, "%d %d\n", ch, '.');
+            }
+        } else if (ch != 10) {
+            if (debug) {
+                fprintf(err, "%d %d\n", ch, ch);
+            }
+            if (subKey == 0 && addKey == 0){
+                fprintf(stdout,"%c" , '.');
+                converted++;
+            }
+        }
+    }
+//	if(debug){
+//		fprintf(stderr, "number of added letters : %d\n", addKey);
+//		fprintf(stderr, "number of removed letters: %d\n" , subKey);
+//		fprintf(stderr, "number of converted letters: %d\n", converted);
+//	}
+	if(toFile){
+		fclose(out);
 	}
-	char ch;
-	while ((ch = fgetc(in)) != EOF){
-		if (subKey){
-			if (ch == 10){
-				if (firstChar == '\0')
-					fprintf(out, "-NONE-\n");
-
-				else{
-					firstChar = '\0';
-					fprintf(out, "\n");
-				}
-				encNum = subKey;
-			}
-			else if (encNum > 0)
-				encNum --;
-			else if (encNum == 0){
-				if (firstChar == '\0')
-					firstChar = ch;
-				fprintf(out, "%c", ch);
-			}
-		}
-		else if (addKey){
-			if (firstChar == '\0')
-				firstChar = ch;
-			if (ch == 10){
-				for (int i = 0; i < encNum; i++)
-					fprintf(out, "%c", firstChar);
-				firstChar = '\0';
-			}
-			fprintf(out,"%c", ch);
-		}
-		else if (ch >= 65 && ch <= 90){
-			if (debug){
-				fprintf(err, "%d ", ch);
-				fprintf(err, "%d\n", '.');
-			}
-			fprintf(out, ".");
-			convertCount++;
-		}
-		else if (ch != 10) {
-			if (debug){
-				fprintf(err,"%d %d\n", ch, ch);
-			}
-			fprintf(out, "%c", ch);
-		}
-		else{
-			fprintf(out, "%c", '\n');
-               }
-
-	}
-				if (debug){
-                		fprintf(err, "the number of letters: %d\n", convertCount);
-				if (in != stdin)
-                                	fprintf(err, "The name of input file: %c", in);
-                        	if (out != stdout)
-                                	fprintf(err, "The name of output file: %c",out);
-			}
-	if (out != stdout)
-        	fclose(out);
-        if (in != stdin)
-                fclose(in);
-	return 0;
+    if(fromFile){
+        fclose(in);
+    }
+    return 0;
 }
+
+
 int hexToNum(char ch){
-	int num = 0;
-	if (ch >= '1' && ch <= '9')
-		num = ch - 0x30;
-	if (ch >= 'A' && ch <= 'F')
-		num = ch - 0x37;
-	return num;
+    int num = 0;
+    if (ch >= '1' && ch <= '9')
+        num = ch - 0x30;
+    if (ch >= 'A' && ch <= 'F')
+        num = ch - 0x37;
+    return num;
 }
